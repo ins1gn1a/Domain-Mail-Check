066ed32c46f88552478b62b6215ee3305d7af616
ins1gn1a <ins1gn1a@users.noreply.github.com>
2017-04-02 11:04:02 +0100

    Added XML output

--- run-dmc.py
+++ run-dmc.py
@@ -4,9 +4,25 @@ import dns.resolver
 import argparse
 import sys
 import tldextract
+from xml.etree.ElementTree import *
+from time import gmtime, strftime
 
-parser = argparse.ArgumentParser(description='SPF Analyser')
+version = "1.0"
+
+no_dmarc = "SC-2166"
+no_spf = "SC-2090"
+full_review = "SC-2157"
+
+ET = ElementTree()
+items_xml = Element('items')
+items_xml.set('version',version)
+items_xml.set('source','Domain Mail Check')
+
+current_time = strftime("dmc-result_%H-%M-%S_%d-%m-%Y", gmtime())
+
+parser = argparse.ArgumentParser(description='SPF and DMARC Analyser')
 parser.add_argument('--domain','-d',help='Enter the Domain name to veriify. E.g. ins1gn1a.com',required=True,nargs='+')
+parser.add_argument('-oX','--xml',required=False,help='Specify the filename to save the XML output (e.g. -oX 123456) or the file will default to the start-time.',default=current_time,dest='xml_job_name')
 args = parser.parse_args()
 
 spf_record = False
@@ -15,7 +31,27 @@ dmarc_record = False
 txt_list = []
 temp_dmarc = []
 
+def xml_file_write(z):
+    with open(args.xml_job_name + ".xml",'w') as xml_file:
+        xml_file.write(z)
+
+
+def vuln_append_print(x):
+    print (x)
+    return (x + "\n")
+
 for domain in args.domain:
+
+    item_xml = SubElement(items_xml, "item", {'ipaddress':'', 'hostname':domain})
+    services_xml = SubElement(item_xml,"services")
+    service_xml = SubElement(services_xml, "service", {'protocol':'tcp', 'port':'', 'name':''})
+    vulnerabilities_services_xml = SubElement(service_xml,"vulnerabilities")
+    vulnerabilities_xml = SubElement(item_xml, 'vulnerabilities')
+
+    vulnerabilities_array = []
+    vuln_review = ""
+
+    dmarc_tld_check = ""
 
     print ("\n")
 
@@ -31,20 +67,32 @@ for domain in args.domain:
             for txt in dns.resolver.query(tld,'TXT').response.answer:
                 txt_list.append(txt.to_text())
         except:
-            sys.exit("[!] No TXT records exist for " + tld)
+            sys.exit("[!] No TXT records exist for " + domain)
 
     txt_list = txt_list[0].split('\n')
 
     # DMARC Checks
     try:
-        extracted = tldextract.extract(domain)
-        tld = "{}.{}".format(extracted.domain, extracted.suffix)
-        for txt in dns.resolver.query(("_dmarc." + tld),'TXT').response.answer:
-            temp_dmarc.append(txt.to_text())
-        if len(temp_dmarc[0]) > 1:
-            txt_list.append(temp_dmarc[0])
+        try:
+            for txt in dns.resolver.query(("_dmarc." + domain),'TXT').response.answer:
+                temp_dmarc.append(txt.to_text())
+            if len(temp_dmarc[0]) > 1:
+                txt_list.append(temp_dmarc[0])
+            dmarc_domain = domain
+
+
+        except:
+            extracted = tldextract.extract(domain)
+            dmarc_domain = "{}.{}".format(extracted.domain, extracted.suffix)
+            dmarc_tld_check = "DMARC checks were performed against '" + dmarc_domain + "' as '" + domain + "' contained no TXT records and/or did not exist."
+            for txt in dns.resolver.query(("_dmarc." + dmarc_domain),'TXT').response.answer:
+                temp_dmarc.append(txt.to_text())
+            if len(temp_dmarc[0]) > 1:
+                txt_list.append(temp_dmarc[0])
+
     except:
-        print ("[!] No DMARC Policy Set Within _dmarc." + tld)
+        print ("[!] No DMARC Policy Set Within _dmarc." + dmarc_domain)
+        vulnerabilities_array.append([no_dmarc, "There were no DMARC records found within the TXT entries for '_dmarc." + dmarc_domain + "'. Implementing DMARC alongside SPF would provide granular control for the management and monitoring of email spoofing. " + dmarc_tld_check])
 
 
     for x in txt_list:
@@ -54,7 +102,7 @@ for domain in args.domain:
             dmarc_record = x
 
     # Main
-    print ("[*] Domain: " + domain)
+    vuln_review = vuln_review + vuln_append_print("[*] Domain: " + domain)
 
     # SPF Checking
 
@@ -71,86 +119,97 @@ for domain in args.domain:
 
     # Process checks against *all
     if spf_record:
-        print ("    [+] SPF: " + spf_record.split("TXT ")[1])
+        vuln_review = vuln_review + vuln_append_print("    [+] SPF: " + spf_record.split("TXT ")[1])
         if "-all" in spf_record:
-            print ("\t[+] Only the following mail servers are authorised to send mail from the " + domain + " domain:")
+            vuln_review = vuln_review + vuln_append_print("\t[+] Only the following mail servers are authorised to send mail from the " + domain + " domain:")
             for z in allowed_servers:
-                print ("\t    - " + z.split(" ")[0])
+                vuln_review = vuln_review + vuln_append_print("\t    - " + z.split(" ")[0])
         elif "~all" in spf_record:
-            print ("\t[+] Only the following mail servers are authorised to send mail from the " + domain + " domain with a soft-fail for non-authorised servers, however '~all' should only be used as a transition to '-all':")
+            vuln_review = vuln_review + vuln_append_print("\t[+] Only the following mail servers are authorised to send mail from the " + domain + " domain with a soft-fail for non-authorised servers, however '~all' should only be used as a transition to '-all':")
             for z in allowed_servers:
-                print ("\t    - " + z.split(" ")[0])
+                vuln_review = vuln_review + vuln_append_print("\t    - " + z.split(" ")[0])
         else:
-            print ("\t[!] The " + domain + " domain is configured in a way that would allow domain email spoofing to be performed.")
+            vuln_review = vuln_review + vuln_append_print("\t[!] The " + domain + " domain is configured in a way that would allow domain email spoofing to be performed.")
         if "redirect:" in spf_record:
-            print ("\t[!] The redirect modifier is configured within the SPF record.")
+            vuln_review = vuln_review + vuln_append_print("\t[!] The redirect modifier is configured within the SPF record.")
     else:
         print ("\t[!] The " + domain + " domain does not utilise SPF records for authorising mail servers and is vulnerable to domain email spoofing.")
+        vulnerabilities_array.append([no_spf, "There were no SPF (Sender Policy Framework) entries found within the TXT records for '" + domain + "'. The domain '" + domain + "' was therefore vulnerable to domain email spoofing."])
 
     if dmarc_record:
-        print ("    [+] DMARC: " + dmarc_record.split("TXT ")[1])
+        vuln_review = vuln_review + vuln_append_print("    [+] DMARC: " + dmarc_record.split("TXT ")[1])
         dmarc_policy_reject = False
         dmarc_params = dmarc_record.split(";")
         for p in dmarc_params:
             # Policy checks: reject, none, quarantine
             if " p=quarantine" in p.lower():
-                print ("\t[+] p=quarantine: Suspicious emails will be marked as suspected SPAM.")
+                vuln_review = vuln_review + vuln_append_print("\t[+] p=quarantine: Suspicious emails will be marked as suspected SPAM.")
             elif " p=reject" in p.lower():
-                print ("\t[+] p=reject: Emails that fail DKIM or SPF checks will be rejected. (Strong)")
+                vuln_review = vuln_review + vuln_append_print("\t[+] p=reject: Emails that fail DKIM or SPF checks will be rejected. (Strong)")
                 dmarc_policy_reject = True
             elif " p=none" in p.lower():
-                print ("\t[-] p=none: No actions will be performed against emails that have failed DMARC checks. (Weak)")
+                vuln_review = vuln_review + vuln_append_print("\t[-] p=none: No actions will be performed against emails that have failed DMARC checks. (Weak)")
 
             # Sender-name (domain/subdomain checks)
             if "adkim=r" in p.lower():
-                print ("\t[-] adkim=r (Relaxed Mode): Emails from *." + domain + " are permitted.")
+                vuln_review = vuln_review + vuln_append_print("\t[-] adkim=r (Relaxed Mode): Emails from *." + domain + " are permitted.")
             elif "adkim=s" in p.lower():
-                print ("\t[+] adkim=s (Strict Mode): Sender domains must match DKIM mail headers exactly. E.g. if 'd=" + domain + "' then emails are not permitted from subdomains. (Strong)")
+                vuln_review = vuln_review + vuln_append_print("\t[+] adkim=s (Strict Mode): Sender domains must match DKIM mail headers exactly. E.g. if 'd=" + domain + "' then emails are not permitted from subdomains. (Strong)")
 
             # Percentage Check 
             if "pct=" in p.lower():
                 percent_val = p.split("=")[1]
-                print ("\t[_] pct=" + percent_val + ": " + percent_val + "% of received mail is subject to DMARC processing")
+                vuln_review = vuln_review + vuln_append_print("\t[_] pct=" + percent_val + ": " + percent_val + "% of received mail is subject to DMARC processing")
 
             if "aspf=r" in p.lower():
-                print ("\t[-] aspf=r (Relaxed Mode): Any sub-domain from " + domain + " are permitted to match DMARC to SPF records.")
+                vuln_review = vuln_review + vuln_append_print("\t[-] aspf=r (Relaxed Mode): Any sub-domain from " + domain + " are permitted to match DMARC to SPF records.")
             elif "aspf=s" in p.lower():
-                print ("\t[+] aspf=s (Strict Mode): The 'header from' domain and SPF must match exactly to pass DMARC checks.")
+                vuln_review = vuln_review + vuln_append_print("\t[+] aspf=s (Strict Mode): The 'header from' domain and SPF must match exactly to pass DMARC checks.")
 
             # Check for SPF/DMARC non-authorsed rejection (No mail)
             if "aspf=s" in p.lower() and spf_allowed_count == 0 and dmarc_policy_reject:
-                print ("\t[!] aspf=s: No email can be sent from the " + domain + " domain. No mail servers authorised in SPF and DMARC rejection enabled.")
+                vuln_review = vuln_review + vuln_append_print("\t[!] aspf=s: No email can be sent from the " + domain + " domain. No mail servers authorised in SPF and DMARC rejection enabled.")
         
             if "rua=" in p.lower():
                 if "mailto:" not in p.lower():
-                    print ("\t[!] rua=: Aggregate mail reports will not be sent as incorrect syntax is used. Prepend 'mailto:' before mail addresses.")
+                    vuln_review = vuln_review + vuln_append_print("\t[!] rua=: Aggregate mail reports will not be sent as incorrect syntax is used. Prepend 'mailto:' before mail addresses.")
                 else:
                     if "," in p:
-                        print ("\t[+] rua=: Aggregate mail reports will be sent to the following email addresses:")
+                        vuln_review = vuln_review + vuln_append_print("\t[+] rua=: Aggregate mail reports will be sent to the following email addresses:")
                         dmarc_mail_list = p.split(",")
                         for dmarc_rua in dmarc_mail_list:
                             try:
                                 dmarc_rua = dmarc_rua.split(":")[1]
                             except:
                                 dmarc_rua = dmarc_rua
-                            print ("\t    - " + dmarc_rua)
+                                vuln_review = vuln_review + vuln_append_print("\t    - " + dmarc_rua)
                     else:
-                        print ("\t[+] rua=" + p[5:] + ": Aggregate mail reports will be sent to this address.")
+                        vuln_review = vuln_review + vuln_append_print("\t[+] rua=" + p[5:] + ": Aggregate mail reports will be sent to this address.")
 
             if "ruf=" in p.lower():
                 if "mailto:" not in p.lower():
-                    print ("\t[!] ruf=: Mail failure reports will not be sent as incorrect syntax is used. Prepend 'mailto:' before mail addresses.")
+                    vuln_review = vuln_review + vuln_append_print("\t[!] ruf=: Mail failure reports will not be sent as incorrect syntax is used. Prepend 'mailto:' before mail addresses.")
                 else:
                     if "," in p:
-                        print ("\t[+] rua=: Mail failure reports will be sent to the following email addresses:")
+                        vuln_review = vuln_review + vuln_append_print("\t[+] rua=: Mail failure reports will be sent to the following email addresses:")
                         dmarc_mail_list = p.split(",")
                         for dmarc_ruf in dmarc_mail_list:
                             try:
                                 dmarc_ruf = dmarc_ruf.split(":")[1]
                             except:
                                 dmarc_ruf = dmarc_ruf
-                            print ("\t    - " + dmarc_ruf)
+                            vuln_review = vuln_review + vuln_append_print("\t    - " + dmarc_ruf)
                     else:
-                        print ("\t[+] rua=" + p[5:] + ": Failure reports sent to this address.")
-                        
+                        vuln_review = vuln_review + vuln_append_print("\t[+] rua=" + p[5:] + ": Failure reports sent to this address.")
 
+
+    for vulnerability_data in vulnerabilities_array:
+        vuln_id,vuln_info = vulnerability_data
+        vuln_xml = SubElement(vulnerabilities_xml,"vulnerability",{'id':vuln_id})
+        vuln_info_xml = SubElement(vuln_xml,"information").text = vuln_info
+
+    if len(vuln_review) > 0:
+        vuln_xml = SubElement(vulnerabilities_xml, "vulnerability", {'id': full_review})
+        vuln_info_xml = SubElement(vuln_xml, "information").text = vuln_review
+
+    xml_file_write((tostring(items_xml, encoding='utf8', method='xml').decode('utf-8')))
